2979b72f9040d867f9d6d8aaeb7eab52424df928
AvivaHebbare23 <eviltoastofdoom@gmail.com>
2026-04-07 10:13:47 +0530

    added acp

---
+++ acpscript.js
@@ -0,0 +1,25 @@
+// function wish() {
+//     var greeting;
+//     var time = new Date().getHours();
+//     if (time < 10) {
+//         greeting = "Good Morning!";
+//     }
+//     else if (time < 20) {
+//         greeting = "Good afternoon!";
+//     }
+//     else {
+//         greeting = "Good evening!";
+//     }
+
+//     document.getElementById("hour").innerHTML = "Current time is: " + time;
+//     document.getElementById("greet").innerHTML = greeting;
+// }
+
+document.write("<hr>")
+var i = 0;
+while (i < 10) {
+    var num = Math.round(Math.random() * 200);
+    document.write(num + "<br>");
+    i = i + 1;
+}
+document.write("<hr>")
